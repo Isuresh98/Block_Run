20aa99df80dc6be5f09a30db60cd4744f3b6d882
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-03-03 14:41:42 +0530

    enemy animation set and complete

--- Assets/Script/EnemyMovement.cs
+++ Assets/Script/EnemyMovement.cs
@@ -6,11 +6,12 @@ public class EnemyMovement : MonoBehaviour
 {
     public AIPath aiPath;
     // Start is called before the first frame update
+    private Animator animator;
 
     Vector2 Driraxtion;
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,8 +24,46 @@ public class EnemyMovement : MonoBehaviour
     {
         Driraxtion = aiPath.desiredVelocity;
 
-        transform.right = Driraxtion;
+       // transform.right = Driraxtion;
 
-       
+        // set the animation movement
+        if (Driraxtion.magnitude > 0.1f) // check if there is any movement
+        {
+            if (Driraxtion.y > 0) // moving up
+            {
+                animator.SetBool("isUp", true);
+                animator.SetBool("isdoun", false);
+                animator.SetBool("isLeft", false);
+                animator.SetBool("isRight", false);
+            }
+            else if (Driraxtion.y < 0) // moving down
+            {
+                animator.SetBool("isUp", false);
+                animator.SetBool("isdoun", true);
+                animator.SetBool("isLeft", false);
+                animator.SetBool("isRight", false);
+            }
+            else if (Driraxtion.x < 0) // moving left
+            {
+                animator.SetBool("isUp", false);
+                animator.SetBool("isdoun", false);
+                animator.SetBool("isLeft", true);
+                animator.SetBool("isRight", false);
+            }
+            else if (Driraxtion.x > 0) // moving right
+            {
+                animator.SetBool("isUp", false);
+                animator.SetBool("isdoun", false);
+                animator.SetBool("isLeft", false);
+                animator.SetBool("isRight", true);
+            }
+        }
+        else // no movement
+        {
+            animator.SetBool("isUp", false);
+            animator.SetBool("isdoun", false);
+            animator.SetBool("isLeft", false);
+            animator.SetBool("isRight", false);
+        }
     }
 }
